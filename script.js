e5a5b25c881f56d7155f506bf04c2ecc973fca73
Amit-Yadav21 <amit21@navgurukul.org>
2023-06-26 17:13:21 +0530

    change ok

--- script.js
+++ script.js
@@ -139,7 +139,7 @@ document.addEventListener("keydown", (event) => {
 function KeyboardInput(key) {
     ClickSound();
 
-    const validKeys = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "+", "-", "*", "/"];
+    const validKeys = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "+", "-", "*", "/","(",")"];
     if (validKeys.includes(key)) {
         display.value += key;
     } else if (key === "Escape" || key === "Delete") {
